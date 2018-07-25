7a4e5b21a401b3d6d55f278bad2c97b95693aa11
Oluwagbemi Kadri <kadri.oluwagbemi@gmail.com>
2018-07-25 08:00:18 +0300

    Weather drop down

--- src/Components/index.js
+++ src/Components/index.js
@@ -132,11 +132,11 @@ class index extends Component {
                     value={weather}                      
                     className="weather_list"
                     > 
-                        <h2> Today </h2>  
+                        <h2 className="header"> Today </h2>  
                         <p> <img src={this.state.imgSrc} alt="WeatherIcon" height="22" width="22" onLoad={this.handleWeatherIcon.bind(this,weather)}/>
                         {weather.weather_state_name} </p>
-                        <p className="weather_prop"> Max: {weather.max_temp} </p>
-                        <p className="weather_prop"> Min:{weather.min_temp} </p>
+                        <p className="weather_prop"> Max: {weather.max_temp}°C </p>
+                        <p className="weather_prop"> Min:{weather.min_temp}°C</p>
                         <p className="weather_prop"> <img src={this.state.imageSrc} alt="WeatherIcon" height="12" width="12" classname="weather_icon"onLoad={this.handleWeatherIcon.bind(this,weather)}/> {weather.wind_speed} mph </p>
                         {/* <p className="weather_prop"> Applicable date: {weather.applicable_date} </p> */}
                         </li>
